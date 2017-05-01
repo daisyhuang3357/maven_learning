ec1064f370831f1b8cb387db2d9f45947c767e4b
Daisy Huang <huangmin3627@163.com>
2017-05-01 12:00:18 +0800

    update

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -15,13 +15,13 @@ public class HelloWorldTest {
         Assert.assertEquals( "Hello Maven", result );
     }
 
-    private String name = "APPLE";
-    @Test
-    public void testSayHello(String name) {
-        HelloWorld helloWorld = new HelloWorld(name);
-
-        String result = helloWorld.sayHello(helloWorld.getName());
-
-        Assert.assertEquals( "Hello " + name, result );
-    }
+//    private String name = "APPLE";
+//    @Test
+//    public void testSayHello(String name) {
+//        HelloWorld helloWorld = new HelloWorld(name);
+//
+//        String result = helloWorld.sayHello(helloWorld.getName());
+//
+//        Assert.assertEquals( "Hello " + name, result );
+//    }
 }
